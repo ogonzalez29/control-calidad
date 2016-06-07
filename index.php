e7189fdc2ec34631bca0692fa12c5447bc367ddb
Daniel Gonzalez <dgonzalez@servitalleres.com>
2016-06-07 16:28:17 -0500

    added mysql database files and configuration in index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require 'ddmenu_config.php'; //Database connection
+require 'connect_db.php'; //Database connection
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
