e7ceb447b8938b0cd7ac74c9cf688a85897ebb8a
Daniel <oscar.d.gonzalez@gmail.com>
2017-06-27 16:23:20 -0500

    added smtp options for mail server when ssl has self-signed certificate option

--- send_email.php
+++ send_email.php
@@ -1,7 +1,7 @@
 <?php
 //Connect to phpmailer script 
 // require_once('assets/phpmailer/class.phpmailer.php');
-require 'assets/PhpMailer/PHPMailerAutoload.php';
+require 'assets/phpmailer/PHPMailerAutoload.php';
 
 //Verify if session started, else redirect to login.php
 ob_start();
@@ -33,14 +33,23 @@ if (isset($_POST['emailSend'])) {
 		$bodytext = $firstname." ".$lastname."<p>Adjunto se encuentra el certificado de control calidad de la reparación hecha a tu vehículo". " ". $make."  ". $line." "."de placas"." ". $license."."." ". "Ten presente las observaciones dadas. Recuerda revisar la política de garantía en el siguiente vínculo: <a href='http://servitalleres.com/politica-de-garantia'>click aquí</a>.<p>Cordial saludo,<p>Servicio al cliente<br>Servitalleres Ltda<br>Carrera 22 # 76-57<br>Tels: 2117943 - 2119290<br><a href='http://servitalleres.com'>www.servitalleres.com</a>";
 
   		//Create a new PHPMailer instance
-	    $mail = new PHPMailer;   
+	    $mail = new PHPMailer();   
 	    $mail->isSMTP();
 		// change this to 0 if the site is going live and 2 if working on localhost or development
-	    $mail->SMTPDebug = 0;
+	    $mail->SMTPDebug = 1;
 	    $mail->Debugoutput = 'html';
 	    $mail->Host = 'smtp.gmail.com';
 	    $mail->Port = 465;
 	    $mail->SMTPSecure = 'ssl';
+
+	    //Options for self-signed certificate in mail server (May not work on every server - OPTIONAL)
+	    $mail->SMTPOptions = array (
+	    	'ssl' => array (
+	        'verify_peer' => false,
+	        'verify_peer_name' => false,
+	        'allow_self_signed' => true
+    		)
+		);
 
 	 	//use SMTP authentication
 	    $mail->SMTPAuth = true;
@@ -51,19 +60,21 @@ if (isset($_POST['emailSend'])) {
 	    $mail->Username = $data['config']['email'][0];
 	    $mail->Password = $data['config']['email'][1];
 	    $mail->setFrom($data['config']['email'][0], 'Servitalleres');
-	    // $mail->addAddress('dgonzalez@servitalleres.com', 'Daniel González');
+	    $mail->addAddress('dgonzalez@servitalleres.com', 'Daniel González');
 	    $mail->Subject = "Certificado de Control Calidad". " ". $make."  ". $line." "."placas"." ". $license;
 	    // $message is gotten from the form
     	$mail->msgHTML($bodytext);
 	
 	    if (!$mail->send()) {
 	        $confMsg = "El correo no fue enviado correctamente, favor intentar de nuevo.";
+	        echo $confMsg;
 	    } else {
 	        $confMsg = "El correo fue enviado correctamente.";
+	        echo $confMsg;
 	        }
 }
 ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
@@ -74,4 +85,4 @@ if (isset($_POST['emailSend'])) {
 		 <div id="text"><?php echo $confMsg;?></div>
 	</div>
 </body>
-</html>
+</html> -->
